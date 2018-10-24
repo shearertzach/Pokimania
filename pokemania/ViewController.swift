acd307bf8f45a1abebd74078e75389e617993c58
Zach <shearertzach@gmail.com>
2018-10-24 09:12:41 -0500

    Initial Commit

---
+++ pokemania/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokemania
+//
+//  Created by Zach Shearer on 10/24/18.
+//  Copyright © 2018 Zach Shearer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
